a95fedbd87ea5a6341180ba2cd5cf75e5e8125f8
sbarton237 <72349665+sbarton237@users.noreply.github.com>
2025-11-08 11:57:43 -0500

    Add Qwiic OLED support and refactor light sensor logic
    Updated the main.py file to include Qwiic OLED setup and modify light sensor reading logic.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 import time
 from machine import Pin, I2C
 import stts22h
+import qwiic_oled
 i2c = I2C(1, sda=Pin(18), scl=Pin(19)) # Correct I2C pins for RP2040
 # for XRP beta use 18 & 19 for sda and scl respectively
 # for XRP use 2 and 3
@@ -16,24 +17,38 @@ else:
     print("Found devices:", [hex(d) for d in devices])
 
 #set up temp
-stts = stts22h.STTS22H(i2c)
-stts.output_data_rate = stts22h.ODR_25_HZ
+# stts = stts22h.STTS22H(i2c)
+# stts.output_data_rate = stts22h.ODR_25_HZ
 
 #set up light
-light_sensor = qwiic_veml6030.QwiicVEML6030()
+light_sensor = qwiic_veml6030.QwiicVEML6030(address=0x48)
 # Check if it's connected
 if light_sensor.is_connected() == False:
     print("The device isn't connected to the system. Please check your connection", \
         file=sys.stderr)
 light_sensor.begin()
 
+#set up oled
+myOLED = qwiic_oled.QwiicMicroOled()
+
+if not myOLED.connected:
+    print("The Qwiic Micro OLED device isn't connected to the system. Please check your connection", \
+        file=sys.stderr)
+myOLED.begin()
+myOLED.clear(myOLED.ALL)
+myOLED.display()
+
 # can change above to be ODR_50_HZ, ODR_100_HZ, and ODR_200_HZ
 while True:
-    for output_data_rate in stts22h.output_data_rate_values:
-        ambient_light = light_sensor.read_light()
-        for _ in range(10):
-            print(f"Temperature: {stts.temperature:.1f}°F")
-            print("Lux:\t%.1f" % ambient_light)
-            time.sleep(0.5)
-        stts.output_data_rate = output_data_rate
+    
+    ambient_light = light_sensor.read_light()
+    for _ in range(10):
+        #print(f"Temperature: {stts.temperature:.1f}°F")
+        print("Lux:\t%.1f" % ambient_light)
+        myOLED.clear(myOLED.PAGE)
+        #myOLED.print(f"Temp: {stts.temperature:.1f}°F")
+        myOLED.print("Lux:\t%.1f" % ambient_light)
+        myOLED.display()
+        time.sleep(0.5)
+        #stts.output_data_rate = output_data_rate
 
