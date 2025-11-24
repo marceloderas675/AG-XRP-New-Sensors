a0e7109fee3cc96c914362ccba942270f0ac9111
sbarton237 <72349665+sbarton237@users.noreply.github.com>
2025-11-24 13:51:40 -0500

    Integrate BME280 and ENS160 sensors
    Added BME280 and ENS160 sensor setup and readings.

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import time
 from machine import Pin, I2C
 import stts22h
 import qwiic_oled
+import qwiic_bme280
+import qwiic_ens160
 i2c = I2C(1, sda=Pin(18), scl=Pin(19)) # Correct I2C pins for RP2040
 # for XRP beta use 18 & 19 for sda and scl respectively
 # for XRP use 2 and 3
@@ -28,6 +30,26 @@ if light_sensor.is_connected() == False:
         file=sys.stderr)
 light_sensor.begin()
 
+#set up bme
+bme_sensor = qwiic_bme280.QwiicBme280()
+
+if bme_sensor.connected == False:
+    print("The Qwiic BME280 device isn't connected to the system. Please check your connection", \
+        file=sys.stderr)
+
+bme_sensor.begin()
+
+#set up ens
+myEns = qwiic_ens160.QwiicENS160()
+
+if myEns.is_connected() == False:
+    print("The device isn't connected to the system. Please check your connection", \
+        file=sys.stderr)
+myEns.begin()
+myEns.set_operating_mode(myEns.kOpModeReset)
+myEns.set_operating_mode(myEns.kOpModeStandard)
+ensStatus = myEns.get_flags()
+
 #set up oled
 myOLED = qwiic_oled.QwiicMicroOled()
 # 
@@ -42,16 +64,24 @@ myOLED.clear(myOLED.ALL)
 while True:
     
     ambient_light = light_sensor.read_light()
-    for output_data_rate in stts22h.output_data_rate_values:
-        print(f"Temperature: {stts.temperature:.1f}°F")
-        print("Lux:\t%.1f" % ambient_light)
-        myOLED.set_cursor(0,0)
-        myOLED.print(f"Temp: {stts.temperature:.1f}°F")
-        myOLED.set_cursor(0,10)
-        myOLED.print("Lux:\t%.1f" % ambient_light)
-        #myOLED.print(_)
-        myOLED.display()
-        myOLED.clear(myOLED.ALL)
-        myOLED.clear(myOLED.PAGE)
-        stts.output_data_rate = output_data_rate
+    print(f"Temperature: {stts.temperature:.1f}°F")
+    print("Lux:\t%.1f" % ambient_light)
+    myOLED.set_cursor(0,0)
+    myOLED.print(f"Temp: {stts.temperature:.1f}°F")
+    myOLED.set_cursor(0,10)
+    myOLED.print("Lux:\t%.1f" % ambient_light)
+    #myOLED.print(_)
+    myOLED.display()
+    print("Humidity:\t%.3f" % bme_sensor.humidity)
+    print("Pressure:\t%.3f" % bme_sensor.pressure)
+    print("Altitude:\t%.3f" % bme_sensor.altitude_feet)
+    print("Temperature:\t%.2f" % bme_sensor.temperature_fahrenheit)
+    if myEns.check_data_status():
+        print("Air Quality Index (1-5) : ", myEns.get_aqi())
+        print("Total Volatile Organic Compounds (ppb): ", myEns.get_tvoc())
+        print("CO2 concentration (ppm): ", myEns.get_eco2())
+        print("Gas Sensor Status Flag (0 - Standard, 1 - Warm up, 2 - Initial Start Up): ", myEns.get_flags())
+    time.sleep(1)
+    myOLED.clear(myOLED.ALL)
+    myOLED.clear(myOLED.PAGE)
 
